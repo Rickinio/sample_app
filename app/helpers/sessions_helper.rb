dc47abe6d575c7c1b9b0d04e914fe8dc2e813682
Ricky Stam <k.stam@hotmail.com>
2012-12-24 18:21:24 +0200

    Friendly forwarding and correct user requirment

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,9 +21,27 @@ module SessionsHelper
 		current_user = nil
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
 	def deny_access
-      redirect_to signin_path, :notice => "You need to Sign In first"
+	    store_location
+	    redirect_to signin_path, :notice => "You need to Sign In first"
     end
+
+    def store_location
+      session[:return_to] = request.fullpath	
+    end
+
+	def redirect_to_back_or(default)
+      redirect_to(session[:return_to] || default )
+      clear_return_to	
+    end
+
+    def clear_return_to
+    	session[:return_to] = nil
+    end    
 
 	private
 
